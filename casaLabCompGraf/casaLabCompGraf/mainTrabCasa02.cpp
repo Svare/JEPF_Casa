d094e60e91c1720a08b01afb59f49d82835b3752
Svare <jepf_27@hotmail.es>
2018-02-19 22:36:01 -0600

    Trabajo en casa práctica 02
    Se dibujo una cereza a partir de una imagen pixelArt tomada de la web.

--- casaLabCompGraf/casaLabCompGraf/mainTrabCasa02.cpp
+++ casaLabCompGraf/casaLabCompGraf/mainTrabCasa02.cpp
@@ -1,6 +1,6 @@
 ///////////////////////////////////////////////////////////////////////////
 // Laboratorio de Computación Gráfica                                  ////
-// Practica # 02                                                       ////
+// Trabajo en casa # 02                                                ////
 // Nombre: Pacheco Franco Jesús Enrique                                ////
 // Version Visual Studio: Visual Studio Community 2017 15.5.27130.2027 ////
 // Grupo: 02                                                           ////
@@ -16,12 +16,27 @@
 void InitGL ( GLvoid )     // Inicializamos parametros
 {
 	//glShadeModel(GL_FLAT);							// Habilitamos Smooth Shading
-	glClearColor(0.0f, 0.0f, 0.0f, 0.0f);				// Negro de fondo
-	//glClearDepth(1.0f);									// Configuramos Depth Buffer
+	glClearColor(1.0f, 1.0f, 1.0f, 1.0f);				// Blanco de fondo
+	//glClearDepth(1.0f);								// Configuramos Depth Buffer
 	//glEnable(GL_DEPTH_TEST);							// Habilitamos Depth Testing
-	//glDepthFunc(GL_LEQUAL);								// Tipo de Depth Testing a realizar
+	//glDepthFunc(GL_LEQUAL);							// Tipo de Depth Testing a realizar
 	
 	
+}
+
+/* Funcion que se encarga de dibujar cuadrados unitarios del color
+	que se le pasa como tercer argumento, toma como centro del cuadrado
+	el vertice que se le pasa en las coordenadas de los dos primeros
+	parametros */
+
+void drawPixel(float xPos, float yPos, float color[3]) {
+	glColor3f(color[0], color[1], color[2]);
+	glBegin(GL_QUADS);
+		glVertex3f(xPos + 0.5f,yPos + 0.5f, -1.2f);
+		glVertex3f(xPos - 0.5f, yPos + 0.5f, -1.2f);
+		glVertex3f(xPos - 0.5f, yPos - 0.5f, -1.2f);
+		glVertex3f(xPos + 0.5f, yPos - 0.5f, -1.2f);
+	glEnd();
 }
 
 void display ( void )   // Creamos la funcion donde se dibuja
@@ -33,110 +48,207 @@ void display ( void )   // Creamos la funcion donde se dibuja
 	//Poner aqui codigo a dibujar
 	
 	/* Si no pongo mis vértices dentro de glBegin y glEnd no se muestran en pantalla */
+	
+	// Colores
 
-	glPointSize(10.0);
-//	glLineWidth(5.0);
-	glColor3f(0.0f, 0.0f, 0.0f);
-/*
-		glBegin(GL_POINTS);
-			glVertex3f(0.0f, 0.0f, -1.2f);
-			glVertex3f(-3.0f, 3.0f, -1.2f);
-			glVertex3f(3.5f, 4.0f, -1.2f);
-			glVertex3f(4.0f, -1.0f, -1.2f);
-		glEnd();
+	float rojoCereza[3] = {1.0f, 0.0f, 0.0f};
+	float verdeHoja[3] = { 0.0f, 0.66f, 0.34f };
+	float negroRama[3] = {0.0f, 0.0f, 0.0f};
 
-		glBegin(GL_LINES);
-			glVertex3f(0.0f, 0.0f, -1.2f);
-			glVertex3f(-3.0f, 3.0f, -1.2f);
-			glVertex3f(3.5f, 4.0f, -1.2f);
-			glVertex3f(4.0f, -1.0f, -1.2f);
-		glEnd();
+	// Fila 01
 
-		glBegin(GL_LINE_STRIP);
-			glVertex3f(0.0f, 0.0f, -1.2f);
-			glVertex3f(-3.0f, 3.0f, -1.2f);
-			glVertex3f(3.5f, 4.0f, -1.2f);
-			glVertex3f(4.0f, -1.0f, -1.2f);
-		glEnd();
+	drawPixel(4.0f, 1.0f, negroRama);
+	drawPixel(5.0f, 1.0f, negroRama);
+	drawPixel(6.0f, 1.0f, negroRama);
+	drawPixel(7.0f, 1.0f, negroRama);
 
-		glBegin(GL_LINE_LOOP);
-			glVertex3f(0.0f, 0.0f, -1.2f);
-			glVertex3f(-3.0f, 3.0f, -1.2f);
-			glVertex3f(3.5f, 4.0f, -1.2f);
-			glVertex3f(4.0f, -1.0f, -1.2f);
-		glEnd();
+	// Fila 02
 
-		glBegin(GL_TRIANGLES);
-			glVertex3f(0.0f, 0.0f, -1.2f);
-			glVertex3f(-3.0f, 3.0f, -1.2f);
-			glVertex3f(3.5f, 4.0f, -1.2f);
-			glVertex3f(4.0f, -1.0f, -1.2f);
-		glEnd();
+	drawPixel(3.0f, 2.0f, negroRama);
+	drawPixel(8.0f, 2.0f, negroRama);
+	drawPixel(13.0f, 2.0f, negroRama);
+	drawPixel(14.0f, 2.0f, negroRama);
+	drawPixel(15.0f, 2.0f, negroRama);
+	drawPixel(16.0f, 2.0f, negroRama);
 
-		glBegin(GL_TRIANGLE_STRIP);
-			glVertex3f(0.0f, 0.0f, -1.2f);
-			glVertex3f(-3.0f, 3.0f, -1.2f);
-			glVertex3f(3.5f, 4.0f, -1.2f);
-			glVertex3f(4.0f, -1.0f, -1.2f);
-		glEnd();
+	drawPixel(4.0f, 2.0f, rojoCereza);
+	drawPixel(5.0f, 2.0f, rojoCereza);
+	drawPixel(6.0f, 2.0f, rojoCereza);
+	drawPixel(7.0f, 2.0f, rojoCereza);
+	
+	// Fila 03
 
-		glBegin(GL_TRIANGLE_STRIP);
-			glVertex3f(0.0f, 0.0f, -1.2f);
-			glVertex3f(-3.0f, 3.0f, -1.2f);
-			glVertex3f(3.5f, 4.0f, -1.2f);
-			glVertex3f(4.0f, -1.0f, -1.2f);
-		glEnd();
+	drawPixel(2.0f, 3.0f, negroRama);
+	drawPixel(9.0f, 3.0f, negroRama);
+	drawPixel(12.0f, 3.0f, negroRama);
+	drawPixel(17.0f, 3.0f, negroRama);
 
-		glBegin(GL_TRIANGLE_FAN);
-			glVertex3f(0.0f, 0.0f, -1.2f);
-			glVertex3f(-3.0f, 3.0f, -1.2f);
-			glVertex3f(3.5f, 4.0f, -1.2f);
-			glVertex3f(4.0f, -1.0f, -1.2f);
-		glEnd();
+	drawPixel(3.0f, 3.0f, rojoCereza);
+	drawPixel(4.0f, 3.0f, rojoCereza);
+	drawPixel(5.0f, 3.0f, rojoCereza);
+	drawPixel(6.0f, 3.0f, rojoCereza);
+	drawPixel(7.0f, 3.0f, rojoCereza);
+	drawPixel(8.0f, 3.0f, rojoCereza);
+	drawPixel(13.0f, 3.0f, rojoCereza);
+	drawPixel(14.0f, 3.0f, rojoCereza);
+	drawPixel(15.0f, 3.0f, rojoCereza);
+	drawPixel(16.0f, 3.0f, rojoCereza);
+	
+	// Fila 04
 
-		glBegin(GL_QUADS);
-			glVertex3f(0.0f, 0.0f, -1.2f);
-			glVertex3f(-3.0f, 3.0f, -1.2f);
-			glVertex3f(3.5f, 4.0f, -1.2f);
-			glVertex3f(4.0f, -1.0f, -1.2f);
-		glEnd();
+	drawPixel(2.0f, 4.0f, negroRama);
+	drawPixel(9.0f, 4.0f, negroRama);
+	drawPixel(11.0f, 4.0f, negroRama);
+	drawPixel(18.0f, 4.0f, negroRama);
 
-		glBegin(GL_POLYGON);
-			glVertex3f(0.0f, 0.0f, -1.2f);
-			glVertex3f(-3.0f, 3.0f, -1.2f);
-			glVertex3f(3.5f, 4.0f, -1.2f);
-			glColor3f(0.0f, 0.0f, 1.0f);
-			glVertex3f(4.0f, -1.0f, -1.2f);
-		glEnd();
-*/
-	glBegin(GL_LINE_LOOP);
-		glVertex3f(3.0f, 4.0f, -1.2f);
-		glVertex3f(3.0f, 6.0f, -1.2f);
-		glVertex3f(-5.0f, 6.0f, -1.2f);
-		glVertex3f(-5.0f, -6.0f, -1.2f);
-		glVertex3f(3.0f, -6.0f, -1.2f);
-		glVertex3f(3.0f, -4.0f, -1.2f);
-		glVertex3f(-3.0f, -4.0f, -1.2f);
-		glVertex3f(-3.0f, 4.0f, -1.2f);
-	glEnd();
+	drawPixel(3.0f, 4.0f, rojoCereza);
+	drawPixel(4.0f, 4.0f, rojoCereza);
+	drawPixel(5.0f, 4.0f, rojoCereza);
+	drawPixel(6.0f, 4.0f, rojoCereza);
+	drawPixel(8.0f, 4.0f, rojoCereza);
+	drawPixel(12.0f, 4.0f, rojoCereza);
+	drawPixel(13.0f, 4.0f, rojoCereza);
+	drawPixel(16.0f, 4.0f, rojoCereza);
+	drawPixel(17.0f, 4.0f, rojoCereza);
 
-	glBegin(GL_QUADS);
-		glColor3f(1.0f, 0.0f, 0.0f);
-			glVertex3f(3.0f, 4.0f, -1.2f);
-			glVertex3f(3.0f, 6.0f, -1.2f);
-			glVertex3f(-5.0f, 6.0f, -1.2f);
-			glVertex3f(-3.0f, 4.0f, -1.2f);
-		glColor3f(0.0f, 1.0f, 0.0f);
-			glVertex3f(-5.0f, 6.0f, -1.2f);
-			glVertex3f(-5.0f, -6.0f, -1.2f);
-			glVertex3f(-3.0f, -4.0f, -1.2f);
-			glVertex3f(-3.0f, 4.0f, -1.2f);
-		glColor3f(0.0f, 0.0f, 1.0f);
-			glVertex3f(-5.0f, -6.0f, -1.2f);
-			glVertex3f(-3.0f, -4.0f, -1.2f);
-			glVertex3f(3.0f, -4.0f, -1.2f);
-			glVertex3f(3.0f, -6.0f, -1.2f);
-	glEnd();
+	// Fila 05
+
+	drawPixel(2.0f, 5.0f, negroRama);
+	drawPixel(9.0f, 5.0f, negroRama);
+	drawPixel(11.0f, 5.0f, negroRama);
+	drawPixel(18.0f, 5.0f, negroRama);
+
+	drawPixel(3.0f, 5.0f, rojoCereza);
+	drawPixel(4.0f, 5.0f, rojoCereza);
+	drawPixel(5.0f, 5.0f, rojoCereza);
+	drawPixel(6.0f, 5.0f, rojoCereza);
+	drawPixel(8.0f, 5.0f, rojoCereza);
+	drawPixel(12.0f, 5.0f, rojoCereza);
+	drawPixel(14.0f, 5.0f, rojoCereza);
+	drawPixel(15.0f, 5.0f, rojoCereza);
+	drawPixel(16.0f, 5.0f, rojoCereza);
+	drawPixel(17.0f, 5.0f, rojoCereza);
+	
+	// Fila 06
+
+	drawPixel(2.0f, 6.0f, negroRama);
+	drawPixel(9.0f, 6.0f, negroRama);
+	drawPixel(11.0f, 6.0f, negroRama);
+	drawPixel(18.0f, 6.0f, negroRama);
+
+	drawPixel(3.0f, 6.0f, rojoCereza);
+	drawPixel(4.0f, 6.0f, rojoCereza);
+	drawPixel(5.0f, 6.0f, rojoCereza);
+	drawPixel(7.0f, 6.0f, rojoCereza);
+	drawPixel(8.0f, 6.0f, rojoCereza);
+	drawPixel(12.0f, 6.0f, rojoCereza);
+	drawPixel(13.0f, 6.0f, rojoCereza);
+	drawPixel(14.0f, 6.0f, rojoCereza);
+	drawPixel(15.0f, 6.0f, rojoCereza);
+	drawPixel(16.0f, 6.0f, rojoCereza);
+	drawPixel(17.0f, 6.0f, rojoCereza);
+
+	// Fila 07
+
+	drawPixel(3.0f, 7.0f, negroRama);
+	drawPixel(8.0f, 7.0f, negroRama);
+	drawPixel(11.0f, 7.0f, negroRama);
+	drawPixel(18.0f, 7.0f, negroRama);
+
+	drawPixel(4.0f, 7.0f, rojoCereza);
+	drawPixel(5.0f, 7.0f, rojoCereza);
+	drawPixel(6.0f, 7.0f, rojoCereza);
+	drawPixel(7.0f, 7.0f, rojoCereza);
+	drawPixel(12.0f, 7.0f, rojoCereza);
+	drawPixel(13.0f, 7.0f, rojoCereza);
+	drawPixel(14.0f, 7.0f, rojoCereza);
+	drawPixel(15.0f, 7.0f, rojoCereza);
+	drawPixel(16.0f, 7.0f, rojoCereza);
+	drawPixel(17.0f, 7.0f, rojoCereza);
+	
+	// Fila 08
+
+	drawPixel(4.0f, 8.0f, negroRama);
+	drawPixel(5.0f, 8.0f, negroRama);
+	drawPixel(6.0f, 8.0f, negroRama);
+	drawPixel(7.0f, 8.0f, negroRama);
+	drawPixel(12.0f, 8.0f, negroRama);
+	drawPixel(17.0f, 8.0f, negroRama);
+
+	drawPixel(13.0f, 8.0f, rojoCereza);
+	drawPixel(14.0f, 8.0f, rojoCereza);
+	drawPixel(15.0f, 8.0f, rojoCereza);
+	drawPixel(16.0f, 8.0f, rojoCereza);
+
+	// Fila 09
+
+	drawPixel(6.0f, 9.0f, negroRama);
+	drawPixel(13.0f, 9.0f, negroRama);
+	drawPixel(14.0f, 9.0f, negroRama);
+	drawPixel(15.0f, 9.0f, negroRama);
+	drawPixel(16.0f, 9.0f, negroRama);
+
+	// Fila 10
+
+	drawPixel(7.0f, 10.0f, negroRama);
+	drawPixel(12.0f, 10.0f, negroRama);
+
+	// Fila 11
+
+	drawPixel(8.0f, 11.0f, negroRama);
+	drawPixel(11.0f, 11.0f, negroRama);
+
+	// Fila 12
+
+	drawPixel(8.0f, 12.0f, negroRama);
+	drawPixel(11.0f, 12.0f, negroRama);
+
+	// Fila 13
+
+	drawPixel(9.0f, 13.0f, negroRama);
+	drawPixel(12.0f, 13.0f, negroRama);
+	drawPixel(13.0f, 13.0f, negroRama);
+	drawPixel(14.0f, 13.0f, negroRama);
+
+	// Fila 14
+
+	drawPixel(10.0f, 14.0f, negroRama);
+	drawPixel(12.0f, 14.0f, negroRama);
+	drawPixel(15.0f, 14.0f, negroRama);
+
+	drawPixel(13.0f, 14.0f, verdeHoja);
+	drawPixel(14.0f, 14.0f, verdeHoja);
+
+	// Fila 15
+
+	drawPixel(10.0f, 15.0f, negroRama);
+	drawPixel(11.0f, 15.0f, negroRama);
+	drawPixel(12.0f, 15.0f, negroRama);
+	drawPixel(16.0f, 15.0f, negroRama);
+
+	drawPixel(13.0f, 15.0f, verdeHoja);
+	drawPixel(14.0f, 15.0f, verdeHoja);
+	drawPixel(15.0f, 15.0f, verdeHoja);
+
+	// Fila 16
+
+	drawPixel(11.0f, 16.0f, negroRama);
+	drawPixel(13.0f, 16.0f, negroRama);
+	drawPixel(16.0f, 16.0f, negroRama);
+
+	drawPixel(14.0f, 16.0f, verdeHoja);
+	drawPixel(15.0f, 16.0f, verdeHoja);
+
+	// Fila 17
+
+	drawPixel(11.0f, 17.0f, negroRama);
+	drawPixel(14.0f, 17.0f, negroRama);
+	drawPixel(15.0f, 17.0f, negroRama);
+	drawPixel(16.0f, 17.0f, negroRama);
+
+	// Fila 18
+
+	drawPixel(17.0f, 18.0f, negroRama);
 
 	glFlush();
 }
@@ -154,7 +266,7 @@ void reshape ( int width , int height )   // Creamos funcion Reshape
 	glLoadIdentity();
 
 	// Ortogonal
-	glOrtho(-10,10,-10,10,0.1,2);
+	glOrtho(-5.0,25.0,-5.0,25,0.1,2);
 
 	glMatrixMode(GL_MODELVIEW);							// Seleccionamos Modelview Matrix
 	glLoadIdentity();									
